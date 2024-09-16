39e269a08a14162ea22e128962bd55f18fd7cff4
kau4r <22102606@usc.edu.ph>
2024-09-16 21:47:57 +0800

    feat: add PUT and DELETE

--- product_service/product_service/urls.py
+++ product_service/product_service/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products/<int:productId>/", views.get_product, name="get-product"),  # New endpoint for getting a specific product
-    path("products/all", views.product_list, name="product-list"),  # Endpoint for listing products
-    path("products", views.create_product, name="create-product"),  # Endpoint for creating a new product
+    path("products/", views.product_view, name="product-list-create"),
+    path("products/<int:productId>/", views.product_view, name="product-detail-update-delete"),
 ]
