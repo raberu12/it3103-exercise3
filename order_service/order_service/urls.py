88249438ea11f3c03836f701d287d4f51a15b548
zenshin <11400071@usc.edu.ph>
2024-09-16 22:21:55 +0800

    FEAT: GET method that lists

--- order_service/order_service/urls.py
+++ order_service/order_service/urls.py
@@ -21,6 +21,7 @@ from django.views.generic.base import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.order_handler, name='order-list'),
     path('orders/', views.order_handler, name='create-order'),
     path('orders/<int:order_id>/', views.order_handler, name='order-handler'),
 ]
