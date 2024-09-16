92510ad84daf2c77704f79229589459246c023a4
kau4r <22102606@usc.edu.ph>
2024-09-16 19:16:53 +0800

    Add GET and POST

--- product_service/product_service/urls.py
+++ product_service/product_service/urls.py
@@ -1,22 +1,8 @@
-"""
-URL configuration for product_service project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("products/<int:productId>/", views.get_product, name="get-product"),  # New endpoint for getting a specific product
+    path("products/all", views.product_list, name="product-list"),  # Endpoint for listing products
+    path("products", views.create_product, name="create-product"),  # Endpoint for creating a new product
 ]
