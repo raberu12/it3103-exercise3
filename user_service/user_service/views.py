c19f1bfff3f02228850dae6a3b38a0fc8192933b
kau4r <22102606@usc.edu.ph>
2024-10-10 07:08:10 +0800

    feat: add roles [admin,user]

--- user_service/user_service/views.py
+++ user_service/user_service/views.py
@@ -3,10 +3,10 @@ from rest_framework.response import Response
 from rest_framework import status
 
 users = [
-    {"id": 1, "username": "Achille", "email": "achille@gmail.com", "password": "achille"},
-    {"id": 2, "username": "Carlo", "email": "carlo@gmail.com", "password": "carloboy"},
-    {"id": 3, "username": "Julz", "email": "julz@gmail.com", "password": "julzstephen"},
-    {"id": 4, "username": "Matt", "email": "matt@gmail.com", "password": "matt0987654321"},
+    {"id": 1, "username": "Achille", "email": "achille@gmail.com", "password": "achille", "role": "user"},
+    {"id": 2, "username": "Carlo", "email": "carlo@gmail.com", "password": "carloboy", "role": "user"},
+    {"id": 3, "username": "Julz", "email": "julz@gmail.com", "password": "julzstephen", "role": "admin"},
+    {"id": 4, "username": "Matt", "email": "matt@gmail.com", "password": "matt0987654321", "role": "admin"},
 ]
 
 @api_view(["GET", "POST", "PUT", "DELETE"])
@@ -26,7 +26,8 @@ def user_view(request, userId=None):
             "id": len(users) + 1,
             "username": user_data.get("username"),
             "email": user_data.get("email"),
-            "password": user_data.get("password")
+            "password": user_data.get("password"),
+            'role': user_data.get('role', 'user')
         }
         if any(value is None for value in new_user.values()):
             return Response({"error": "Username, email, and password must have values."}, status=status.HTTP_400_BAD_REQUEST)
@@ -43,6 +44,7 @@ def user_view(request, userId=None):
         user["username"] = user_data.get("username", user["username"])
         user["email"] = user_data.get("email", user["email"])
         user["password"] = user_data.get("password", user["password"])
+        user["role"] = user_data.get("role", user["role"])
         if any(value is None for value in user.values()):
             return Response({"error": "Username, email, and password must have values."}, status=status.HTTP_400_BAD_REQUEST)
         return Response({k: v for k, v in user.items() if k != 'password'}, status=status.HTTP_200_OK)
